f7f79d50ee8a735cd20285e26a061a756f0db74a
jx4515ga <jx4515ga@go.minnstate.edu>
2023-02-22 18:07:43 -0600

    countdown timer program 1st commit

---
+++ READ.md
@@ -0,0 +1,8 @@
+This is a simple countdown timer program in python. 
+
+The application will ask user to enter time in seconds, at the end of countdown 
+the application will display a message to alert user.
+
+Timer is set up to dysplay in the following format (hours / minutes / seconds)
+00:00:12
+image.png
---
+++ countdowntimer.py
@@ -0,0 +1,27 @@
+
+import time
+
+
+
+#user input time in seconds
+user_time = input("How long would you like set the timer for? : ")
+
+
+# define the countdown function, using the user time in seconds.
+def countdown(user_time):
+    
+    while user_time:
+        mins, secs = divmod(user_time, 60)
+        hours = int(user_time / 3600)
+        timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        user_time -= 1
+    #message to aler user
+    print('Wake up! Times up')
+  
+  
+
+  
+
+countdown(int(user_time))
